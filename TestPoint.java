933b123d698bc27d3dab161076d149a613bb0b25
babububu <1010862883@qq.com>
2017-03-12 17:00:59 +0800

    Update TestPoint.java

--- TestPoint.java
+++ TestPoint.java
@@ -21,16 +21,17 @@ class Point {
 		z = _z;
 	}
 	
-	public int distance() {
-		int d = x*x + y*y + z*z;
+	public int distance(Point p) {
+		int d = (x - p.x)*(x - p.x) + (y - p.y)* (y - p.y) + (z - p.z)*(z - p.z);
 		return d;
 	}
 }
 
 public class TestPoint {
 	public static void main(String[] args) {
-		Point p = new Point(1,2,3);
-		int dis = p.distance();
+                Point p = new Point(1,2,3);
+		Point p1 = new Point(3,4,5);
+		int dis = p.distance(p1);
 		System.out.println(dis);
 	}
 }
